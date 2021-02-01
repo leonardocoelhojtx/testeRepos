6129a921984e9a4c692903fa2655ef24aa9486dd
leonardocoelhojtx <leonardo.coelho@justax.com.br>
2021-02-01 10:35:46 -0300

    Commit

---
+++ Frontend/FrontendAdmin/node_modules/@coreui/angular/lib/breadcrumb/app-breadcrumb.module.d.ts
@@ -0,0 +1,13 @@
+import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './app-breadcrumb.component';
+import * as ɵngcc2 from './cui-breadcrumb.component';
+import * as ɵngcc3 from '@angular/common';
+import * as ɵngcc4 from '@angular/router';
+export declare class AppBreadcrumbModule {
+    static forRoot(config?: any): ModuleWithProviders<AppBreadcrumbModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<AppBreadcrumbModule, [typeof ɵngcc1.AppBreadcrumbComponent, typeof ɵngcc2.CuiBreadcrumbComponent], [typeof ɵngcc3.CommonModule, typeof ɵngcc4.RouterModule], [typeof ɵngcc1.AppBreadcrumbComponent, typeof ɵngcc2.CuiBreadcrumbComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<AppBreadcrumbModule>;
+}
+
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLWJyZWFkY3J1bWIubW9kdWxlLmQudHMiLCJzb3VyY2VzIjpbImFwcC1icmVhZGNydW1iLm1vZHVsZS5kLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOzs7Ozs7QUFDQTs7OztBQUVBIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgTW9kdWxlV2l0aFByb3ZpZGVycyB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xyXG5leHBvcnQgZGVjbGFyZSBjbGFzcyBBcHBCcmVhZGNydW1iTW9kdWxlIHtcclxuICAgIHN0YXRpYyBmb3JSb290KGNvbmZpZz86IGFueSk6IE1vZHVsZVdpdGhQcm92aWRlcnM8QXBwQnJlYWRjcnVtYk1vZHVsZT47XHJcbn1cclxuIl19
